34d0cb6fe4514cf31db9a57e094414d2ea1e1665
antonio.torre <antonio.torre@pagopa.it>
2024-12-03 15:19:48 +0100

    cleaning dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,42 +30,24 @@ repositories {
 
 val springDocOpenApiVersion = "2.6.0"
 val openApiToolsVersion = "0.2.6"
-val findbugsVersion = "3.0.2"
-val javaJwtVersion = "4.4.0"
-val jwksRsaVersion = "0.22.1"
-val nimbusJoseJwtVersion = "9.47"
-val jjwtVersion = "0.12.6"
-val wiremockVersion = "3.9.2"
-val wiremockSpringBootVersion = "2.1.3"
 val micrometerVersion = "1.4.0"
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	implementation("org.springframework.boot:spring-boot-starter-security")
   implementation("io.micrometer:micrometer-tracing-bridge-otel:$micrometerVersion")
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:$springDocOpenApiVersion")
 	implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
 	implementation("org.openapitools:jackson-databind-nullable:$openApiToolsVersion")
-	implementation("com.google.code.findbugs:jsr305:$findbugsVersion")
 
 	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
 
-	// validation token jwt
-	implementation("com.auth0:java-jwt:$javaJwtVersion")
-	implementation("com.auth0:jwks-rsa:$jwksRsaVersion")
-	implementation("com.nimbusds:nimbus-jose-jwt:$nimbusJoseJwtVersion")
-	implementation("io.jsonwebtoken:jjwt-api:$jjwtVersion")
-
 	//	Testing
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.springframework.security:spring-security-test")
 	testImplementation("org.mockito:mockito-core")
 	testImplementation ("org.projectlombok:lombok")
-	testImplementation ("org.wiremock:wiremock-standalone:$wiremockVersion")
-	testImplementation ("com.maciejwalkowiak.spring:wiremock-spring-boot:$wiremockSpringBootVersion")
 }
 
 tasks.withType<Test> {
