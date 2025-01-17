809be0ef7394df1de87271d346f07267f547998b
antonio.torre <antonio.torre@pagopa.it>
2025-01-17 20:09:54 +0100

    disabling errorMvc

--- src/main/java/it/gov/pagopa/template/TemplateApplication.java
+++ src/main/java/it/gov/pagopa/template/TemplateApplication.java
@@ -2,8 +2,9 @@ package it.gov.pagopa.template;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
 public class TemplateApplication {
 
 	public static void main(String[] args) {
