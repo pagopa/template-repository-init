0318f22262224982b966dcd34890fbecde93ad41
antonio.torre <antonio.torre@pagopa.it>
2025-02-10 09:53:46 +0100

    chore: Swagger configured through annotation

--- src/main/java/it/gov/pagopa/template/config/SwaggerConfig.java
+++ src/main/java/it/gov/pagopa/template/config/SwaggerConfig.java
@@ -1,41 +1,18 @@
 package it.gov.pagopa.template.config;
 
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * The Class SwaggerConfig.
  */
 @Configuration
+@OpenAPIDefinition(
+  info = @io.swagger.v3.oas.annotations.info.Info(
+    title = "${spring.application.name}",
+    version = "${spring.application.version}",
+    description = "Api and Models"
+  )
+)
 public class SwaggerConfig {
-
-  /** The title. */
-  private final String title;
-
-  /** The description. */
-  private final String description;
-
-  /** The version. */
-  private final String version;
-
-  public SwaggerConfig(
-    @Value("${swagger.title:${spring.application.name}}") String title,
-    @Value("${swagger.description:Api and Models}") String description,
-    @Value("${swagger.version:${spring.application.version}}") String version) {
-    this.title = title;
-    this.description = description;
-    this.version = version;
-  }
-
-  @Bean
-  public OpenAPI customOpenAPI() {
-    return new OpenAPI().components(new Components()).info(new Info()
-        .title(title)
-        .description(description)
-        .version(version));
-  }
 }
